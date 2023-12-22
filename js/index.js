d741f2f8fe4f4bd67d442f44f0d320dc758a35c3
martin godoy <martingodoy001@hotmail.com>
2023-12-22 13:20:33 -0300

    Se agrego y soluciono errores

--- js/index.js
+++ js/index.js
@@ -1,42 +1,60 @@
-const menu = parseInt(prompt("Buen dia a continuacion le presentamos nuestro programa coloque: 1) Para conversor de divisas 2) Para calculadora corriente ... Cualquier otro numero para salir!"))
+let menu = parseInt(prompt("\n1) Para conversor de divisas\n2) Ver historial\n3) Para salir!"));
+const historial = [];
+let dolares = 804;
+let euros = 887;
 
-let continuar = true
-
-
-switch (menu) {
-    case 1:
-        let cantidadDivisa = parseInt(prompt("Ingrese una cantidad: "))
-        let elegirMoneda = prompt("Desea convertir dolares o euros?").toLowerCase
-        let dolares = 360
-        let euros = 390
-        
-        function conversor(cantidadDivisa, elegirMoneda) {
-        if (elegirMoneda === dolares){
-            return (console.log(cantidadDivisa / dolares));
-        } else if (elegirMoneda === euros){
-            return (console.log(cantidadDivisa / euros));
-        } else {
-            return
-            }
-        }
-        conversor()
-        break;
-
-    case 2:
-        while (continuar) {
-        let multiplicar = parseInt(prompt("Ingrese numero a multiplicar del 1 al 10: "))
-        for (let numeros = 1; numeros <=10; numeros++){
-            let resultado = numeros*multiplicar
-            console.log(multiplicar + "x" +numeros+ ": "+ resultado);
-        }
-        let confirmacion = prompt("Desea hacer otro calculo?").toLocaleLowerCase
-        if (confirmacion == "no") {
-            continuar = false
-            console.log("Gracias por usar nuestra calculadora");
-            break
-        }
+class Operacion {
+    constructor(operacion, mensaje) {
+        this.operacion = operacion;
+        this.mensaje = mensaje;
     }
 }
 
+function verHistorialFiltrado() {
+    let filtroMoneda = prompt("Ingrese el tipo de operación que desea ver (dolares o euros):").toLowerCase();
+    let mensaje = "";
+
+    historial.forEach((el) => {
+        if (el.operacion.toLowerCase() === filtroMoneda) {
+            mensaje = mensaje + el.mensaje + "\n";
+        }
+    });
+
+    if (mensaje === "") {
+        alert(`No hay operaciones registradas para ${filtroMoneda}.`);
+    } else {
+        alert(mensaje);
+    }
+}
+
+while (menu != 3) {
+    switch (menu) {
+        case 1:
+            let cantidadDivisa = parseInt(prompt("Ingrese una cantidad: "));
+            let elegirMoneda = prompt("Desea convertir dolares o euros?").toLowerCase();
+
+            function conversor(cantidadDivisa, elegirMoneda) {
+                let resultado;
+                if (elegirMoneda === "dolares") {
+                    resultado = cantidadDivisa / dolares;
+                    console.log("Tus pesos serian: $" + resultado + " dolares.");
+                } else if (elegirMoneda === "euros") {
+                    resultado = cantidadDivisa / euros;
+                    console.log("Tus pesos serian: $" + resultado + " euros.");
+                } else {
+                    console.log("Moneda no valida.");
+                }
+                const historialNew = new Operacion(elegirMoneda, `\n-La divisa fue de: $${cantidadDivisa} en la moneda: ${elegirMoneda} \n- Resultado: $${resultado} ${elegirMoneda}`)
+                historial.push(historialNew)
+            }
+            conversor(cantidadDivisa, elegirMoneda);
+            break;
+
+        case 2:
+            verHistorialFiltrado();
+            break;
+    }
+    menu = parseInt(prompt("\n1) Para conversor de divisas\n2) Ver historial\n 3) Para salir!"));
+}
 
 
